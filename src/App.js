2fd040ec515bca968e28699f68d04bba52b68f91
$$$Sakthi Nathan$$$ <45733926+NathanDrake007@users.noreply.github.com>
2021-05-11 19:01:37 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Checkout from "./presentation/components/checkout/Checkout";
+import Blog from "./presentation/pages/BlogPage";
+import SignUp from "./presentation/pages/SignUpPage";
+function App() {
+  return (
+    <div className="App">
+      {/* <SignUp /> */}
+      {/* <Blog /> */}
+      <Checkout />
+    </div>
+  );
+}
+
+export default App;
