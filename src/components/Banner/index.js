31d24b6a23199c3253d3dbbc9dd93a37d1703f1c
$$$Sakthi Nathan$$$ <drakesathi@gmail.com>
2021-08-09 17:01:31 +0530

    refactoring

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,9 +4,7 @@ import "./banner.css";
 function Banner(props) {
   return (
     <section className="banner">
-      <div className="">
         <img className="banner-image" src={props.image} alt="banner" />
-      </div>
     </section>
   );
 }
--- src/pages/ProductPage/index.js
+++ src/pages/ProductPage/index.js
@@ -4,8 +4,47 @@ import Banner from "../../components/Banner";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 import "./product_page.css";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Grid from "@material-ui/core/Grid";
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Container } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    [theme.breakpoints.up('md')]: {
+    display: "flex",
+    },
+  },
+grid:{
+  padding: "25px 100px 50px 100px",
+  [theme.breakpoints.down('md')]: {
+    padding: "0px 50px 15px 50px"
+  },
+},
+  cardImage:{
+    objectFit: "contain",
+    margin: "10px",
+    [theme.breakpoints.down('md')]: {
+      margin: "0px -5px 10px 0px",
+    },
+  },
+  cardImage1:{
+      objectFit: "contain",
+      margin: "10px 10px 10px -10px",
+      [theme.breakpoints.down('md')]: {
+        margin: "10px -5px 0px 0px",
+      },
+  },
+}));
 function ProductPage(props) {
   const [product, setProduct] = useState(null);
+  const classes = useStyles();
   useEffect(() => {
     async function fetchProducts() {
       await firestore
@@ -26,7 +65,7 @@ function ProductPage(props) {
     // props.addToCart({ ...props.product, quantity: 1 });
     // console.log({ userId: props.userId, product: title });
   };
-  const renderPage = () => {
+  const renderPage = () => { 
     return (
       <>
         <NavBar />
@@ -36,92 +75,127 @@ function ProductPage(props) {
             description={product.feature}
             image={product.bannerImage}
           />
-          <div className="row">
-            <div className="col-md-12">
-              <div className="container my-3 d-flex align-items-center flex-row-reverse bg-white justify-content-around">
-                <img src={product.image} alt="feature-1" width="500" />
-                <div className="text-center">
-                  <h1 className="fs-1">{product.title}</h1>
-                  <p className="fs-5">{product.description_1}</p>
-                  <span
-                    onClick={handleBuy}
-                    style={{ cursor: "pointer", color: "lightblue" }}
-                  >
-                    Buy&nbsp;<i class="fa fa-angle-right"></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-12">
-              <div className="container">
-                <div className="row">
-                  <div
-                    className="col p-4 bg-white me-3"
-                    style={{ height: "600px" }}
-                  >
-                    <div
-                      className="h-100 text-center"
-                      style={{
-                        backgroundImage: `url(${product.image})`,
-                        backgroundPosition: "top",
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "cover",
-                      }}
-                    >
-                      <h2 className="fs-2">{product.title}</h2>
-                      <h5 className="fs-5">{product.description_2}</h5>
-                      <div class="links small-link-margin">
-                        <span
-                          onClick={handleBuy}
-                          style={{ cursor: "pointer", color: "blue" }}
-                        >
-                          Buy&nbsp;<i class="fa fa-angle-right"></i>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col p-4 bg-white" style={{ height: "600px" }}>
-                    <div
-                      className="h-100 text-center"
-                      style={{
-                        backgroundImage: `url(${product.image})`,
-                        backgroundPosition: "top",
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "cover",
-                      }}
-                    >
-                      <h2 className="fs-2">{product.title}</h2>
-                      <h5 className="fs-5">{product.description_3}</h5>
-                      <div class="links small-link-margin">
-                        <span
-                          onClick={handleBuy}
-                          style={{ cursor: "pointer", color: "blue" }}
-                        >
-                          Buy&nbsp;<i class="fa fa-angle-right"></i>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <Grid container spacing={3} className={classes.grid}>
+            <Grid item xs={12}>
+            <Card className={classes.root}>
+        <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+          {product.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          {product.description_1}
+          </Typography>
+        </CardContent>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary" onClick={handleBuy}>
+          Buy Now
+        </Button>
+      </CardActions>
+      </CardActionArea>
+      <Container>
+        <CardMedia
+            component="img"
+            alt="beardWax"
+            image={product.images[0]}
+            title="beardWax"
+            className={classes.cardImage}
+          />
+      </Container>
+    </Card>
+    </Grid>
+    <Grid item xs={12} sm={6}>
+      <Card>
+        <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+          {product.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          {product.description_1}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary" onClick={handleBuy}>
+            Buy Now
+          </Button>
+        </CardActions>
+      </CardActionArea>
+        <CardMedia
+          component="img"
+          alt="beardWax"
+          image={product.images[2]}
+          title="beardWax"
+        />
 
-            <div className="col-md-12">
-              <div className="container my-3 d-flex align-items-center flex-row-reverse bg-white justify-content-around">
-                <img src={product.image} alt="feature-1" width="500" />
-                <div className="text-center">
-                  <h1 className="fs-1">{product.title}</h1>
-                  <p className="fs-5">{product.description_1}</p>
-                  <span
-                    onClick={handleBuy}
-                    style={{ cursor: "pointer", color: "lightblue" }}
-                  >
-                    Buy&nbsp;<i class="fa fa-angle-right"></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
+    </Card>
+    </Grid>
+    <Grid item xs={12} sm={6}>
+      <Card>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+          {product.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          {product.description_1}
+          </Typography>
+        </CardContent>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary" onClick={handleBuy}>
+          Buy Now
+        </Button>
+      </CardActions>
+      </CardActionArea>
+        <CardMedia
+          component="img"
+          alt="beardWax"
+          image={product.images[3]}
+          title="beardWax"
+        />
+    </Card>
+    </Grid>
+    <Grid item xs={12}>
+      <Card className={classes.root}>
+          <Container>
+            <CardMedia
+              component="img"
+              alt="beardWax"
+              image={product.images[1]}
+              title="beardWax"
+              className={classes.cardImage1}
+            />
+          </Container>
+        <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+          {product.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          {product.description_4}
+          </Typography>
+        </CardContent>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary" onClick={handleBuy}>
+          Buy Now
+        </Button>
+      </CardActions>
+      </CardActionArea>
+    </Card>
+    </Grid>
+    </Grid>
         </div>
         <Footer />
       </>
